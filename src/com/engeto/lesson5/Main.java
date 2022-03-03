2cc7dc0f40ade2e301017a9e302db23aa4804dc4
MiroslavHorejsi <mira.horejsi@gmail.com>
2022-03-03 20:49:13 +0100

    added validation of watering frequency and watering date (created PlantException class and exceptions0

--- src/com/engeto/lesson5/Main.java
+++ src/com/engeto/lesson5/Main.java
@@ -1,14 +1,34 @@
 package com.engeto.lesson5;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Plant plant1 = new Plant("Fíkus");
+        Plant plant1 = null;
+        Plant plant2 = null;
 
-        System.out.println(plant1);
+        try {
+            plant1 = new Plant("Fíkus");
+            plant2 = new Plant("Bazalka", "na vaření", LocalDate.of(2022,01,12),
+                    LocalDate.of(2022, 01,12), 0);
 
-        System.out.println(plant1.getWateringInfo());
+        } catch (PlantException e) {
+            System.err.println(e.getMessage());;
+        }
+
+        if(plant1 != null){
+            System.out.println(plant1);
+            System.out.println(plant1.getWateringInfo());
+        }
+
+
+        if(plant2 != null){
+            System.out.println(plant2);
+            System.out.println(plant2.getWateringInfo());
+        }
+
 
     }
 }
